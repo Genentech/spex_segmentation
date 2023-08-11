e8afa9524b80364f242aa3720397d063838d65d3
Aleksandr Prilipko <zverozabr@yandex.ru>
2023-08-11 14:35:46 +0700

    tested fixed, anndata saving, support dependence script, cocncat to old version, old version will be deleted late

--- feature_extraction/app.py
+++ feature_extraction/app.py
@@ -1,10 +1,13 @@
-from skimage.measure import regionprops_table
+from skimage.measure import regionprops_table, regionprops
+from skimage import measure
+from anndata import AnnData
+from vitessce.data_utils import to_uint8, optimize_adata
 import pandas as pd
+import numpy as np
 import re
 
 
 def feature_extraction(img, labels, channel_list, all_channels):
-
     """Extract per cell expression for all channels
 
     Parameters
@@ -51,10 +54,72 @@ def feature_extraction(img, labels, channel_list, all_channels):
     return per_cell_data_df
 
 
-def run(**kwargs):
+def feature_extraction_adata(img, labels, channelnames):
+    """Extract per cell expression for all channels
 
+    Parameters
+    ----------
+    img : Multichannel image as numpy array
+    labels: 2d segmentation label numpy array
+    channelnames: list containing channel names (in same order as numpy array!!!)
+
+    Returns
+    -------
+    perCellanndata: anndata single-cell object with all data
+
+    """
+
+    label_array = labels
+
+    props = measure.regionprops_table(label_array, intensity_image=np.transpose(img, (1, 2, 0)),
+                                      properties=['label', 'area', 'centroid', 'mean_intensity'])
+    perCellData = pd.DataFrame(props)
+
+    perCellData.columns = ['cell_id', 'area_pixels', 'Y', 'X'] + channelnames  # rename columns
+
+    coordinates = np.array([k for k in perCellData[['X', 'Y']].values.tolist()])
+
+    props = regionprops(label_array)
+    ordered_contours = []
+    for region in props:
+        # Find contours of the region
+        contours = measure.find_contours(label_array == region.label, 0.5)
+
+        if len(contours) > 0:
+            # Select the contour with the largest area
+            contour = max(contours, key=lambda contour: contour.shape[0])
+        else:
+            contour = contours[0]
+
+        centroid = np.mean(contour, axis=0)
+
+        # Calculate angles of the points with respect to the centroid
+        angles = np.arctan2(contour[:, 0] - centroid[0], contour[:, 1] - centroid[1])
+
+        # Sort points based on angles
+        sorted_indices = np.argsort(angles)
+        sorted_contour = contour[sorted_indices]
+
+        ordered_contours.append(sorted_contour)
+
+    adata = AnnData(perCellData, obsm={"spatial": coordinates})
+    adata.obsm['cell_polygon'] = np.array(ordered_contours, dtype=object)
+
+    adata.obs['Cell_ID'] = perCellData[['cell_id']].values
+    adata.obs['Nucleus_area'] = perCellData[['area_pixels']].values
+    adata.obs['x_coordinate'] = perCellData[['X']].values
+    adata.obs['y_coordinate'] = perCellData[['Y']].values
+
+    adata.layers['X_uint8'] = to_uint8(adata.X, norm_along="global")  # vitessce only supports 8bit expression
+    return adata
+
+
+def run(**kwargs):
     image = kwargs.get('image')
     labels = kwargs.get('labels')
+    all_channels = kwargs.get("all_channels", [])
+    adata = feature_extraction_adata(image, labels, all_channels)
+
     channel_list = kwargs.get("channel_list", [])
     channel_list = [re.sub("[^0-9a-zA-Z]", "", item).lower().replace("target", "") for item in channel_list]
     all_channels = kwargs.get("all_channels", [])
@@ -70,4 +135,4 @@ def run(**kwargs):
 
     df = feature_extraction(image, labels, channel_list, all_channels)
 
-    return {'dataframe': df}
+    return {'adata': adata, 'dataframe': df}
