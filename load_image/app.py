803b4b0558a4ce2e5724b9970183f17d8cd7ebe3
Aleksandr Prilipko <zverozabr@yandex.ru>
2023-09-14 13:30:22 +0700

    changed load tiff -> load image

--- load_image/app.py
+++ load_image/app.py
@@ -1,9 +1,10 @@
 from aicsimageio import AICSImage
-from aicsimageio.writers import OmeTiffWriter,OmeZarrWriter
+from aicsimageio.writers import OmeTiffWriter, OmeZarrWriter
 from tifffile import imread, imsave, TiffWriter, imwrite, TiffFile
 import json
 
 import re
+
 
 def convert_mibitiff2zarr(inputpath, outputpath):
     """convert mibi tiff to zarr
@@ -12,31 +13,32 @@ def convert_mibitiff2zarr(inputpath, outputpath):
     ----------
     inputpath : Path of tiff file
     outputpath : Path of ometiff or omezarr file. Note: for omezarr, the path must end in *.zarr/0
-    
+
     """
     img = AICSImage(inputpath)
-    im_array=img.get_image_data("ZYX", T=0,C=0)
-    
+    im_array = img.get_image_data("ZYX", T=0, C=0)
+
     Channel_list = []
     with TiffFile(inputpath) as tif:
         for page in tif.pages:
             # get tags as json
             description = json.loads(page.tags['ImageDescription'].value)
             Channel_list.append(description['channel.target'])
-    
+
     writer = OmeZarrWriter(output)
-    writer.write_image(im_array, image_name="Image:0", dimension_order="CYX",channel_names=Channel_list,scale_num_levels=4,physical_pixel_sizes=None,channel_colors=None)
-    
+    writer.write_image(im_array, image_name="Image:0", dimension_order="CYX", channel_names=Channel_list,
+                       scale_num_levels=4, physical_pixel_sizes=None, channel_colors=None)
+
     print('conversion complete')
-    
+
+
 def load_image(imgpath):
-    
     """Load image and check/correct for dimension ordering
 
     Parameters
     ----------
     img : Path of ometiff or omezarr file. Note: for omezarr, the path must end in *.zarr/0
-    
+
     Returns
     -------
     Image Stack : 2D numpy array
@@ -45,27 +47,27 @@ def load_image(imgpath):
     """
 
     img = AICSImage(imgpath)
-    
-    dims=['T','C','Z']
-    shape=list(img.shape)
-    channel_dim=dims[shape.index(max(shape[0:3]))]
 
-    array=img.get_image_data(channel_dim+"YX")
+    dims = ['T', 'C', 'Z']
+    shape = list(img.shape)
+    channel_dim = dims[shape.index(max(shape[0:3]))]
 
-    channel_list=img.channel_names
+    array = img.get_image_data(channel_dim + "YX")
+
+    channel_list = img.channel_names
 
     if len(channel_list) != array.shape[0]:
-        channel_list=[]
+        channel_list = []
         with TiffFile(imgpath) as tif:
             for page in tif.pages:
                 # get tags as json
                 description = json.loads(page.tags['ImageDescription'].value)
                 channel_list.append(description['channel.target'])
-    
+
     return array, channel_list
 
-def run(**kwargs):
 
+def run(**kwargs):
     image = kwargs.get('image_path')
     image, channel_list = load_image(image)
 
